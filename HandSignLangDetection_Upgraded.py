ccfcc79ec4759befc6a4fbd07f60fe9bc470f08d
FatmaShafik <122888363+FatmaShafik@users.noreply.github.com>
2023-01-19 21:06:29 +0200

    Add files via upload

--- HandSignLangDetection_Upgraded.py
+++ HandSignLangDetection_Upgraded.py
@@ -1,5 +1,14 @@
 import cv2
 import mediapipe as mp
+import pyttsx3
+
+engine = pyttsx3.init()
+
+
+def AI_speak(label):
+    engine.say(label)
+    engine.runAndWait()
+
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
@@ -30,10 +39,10 @@ while True:
             for tip in finger_tips:
                 x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
                 # print(id, ":", x, y)
-                #cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
+                # cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
 
                 if lm_list[tip].x < lm_list[tip - 2].x:
-                    #cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
+                    # cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
                     finger_fold_status.append(True)
                 else:
                     finger_fold_status.append(False)
@@ -48,47 +57,55 @@ while True:
                     lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[0].x < \
                     lm_list[5].x:
                 cv2.putText(img, "STOP", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-                print("STOP")
+                label = "STOP"
+                AI_speak(label)
 
             # Forward
             if lm_list[3].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y > lm_list[10].y and \
                     lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y:
                 cv2.putText(img, "FORWARD", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-                print("FORWARD")
+                label = "FORWARD"
+                AI_speak(label)
 
             # Backward
-            if lm_list[3].x > lm_list[4].x and lm_list[3].y < lm_list[4].y and lm_list[8].y > lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+            if lm_list[3].x > lm_list[4].x and lm_list[3].y < lm_list[4].y and lm_list[8].y > lm_list[6].y and lm_list[
+                12].y < lm_list[10].y and \
                     lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y:
                 cv2.putText(img, "BACKWARD", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-                print("BACKWARD")
+                label = "BACKWARD"
+                AI_speak(label)
 
             # Left
             if lm_list[4].y < lm_list[2].y and lm_list[8].x < lm_list[6].x and lm_list[12].x > lm_list[10].x and \
                     lm_list[16].x > lm_list[14].x and lm_list[20].x > lm_list[18].x and lm_list[5].x < lm_list[0].x:
                 cv2.putText(img, "LEFT", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-                print("LEFT")
+                label = "LEFT"
+                AI_speak(label)
 
             # Right
             if lm_list[4].y < lm_list[2].y and lm_list[8].x > lm_list[6].x and lm_list[12].x < lm_list[10].x and \
                     lm_list[16].x < lm_list[14].x and lm_list[20].x < lm_list[18].x:
                 cv2.putText(img, "RIGHT", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-                print("RIGHT")
-
+                label = "RIGHT"
+                AI_speak(label)
 
             if all(finger_fold_status):
                 # like
-                if lm_list[thumb_tip].y < lm_list[thumb_tip - 1].y < lm_list[thumb_tip - 2].y and lm_list[0].x < lm_list[3].y:
-                    print("LIKE")
+                if lm_list[thumb_tip].y < lm_list[thumb_tip - 1].y < lm_list[thumb_tip - 2].y and lm_list[0].x < \
+                        lm_list[3].y:
+                    label = "LIKE"
+                    AI_speak(label)
                     cv2.putText(img, "LIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
                     # h, w, c = like_img.shape
                     # img[35:h + 35, 30:w + 30] = like_img
                 # Dislike
-                if lm_list[thumb_tip].y > lm_list[thumb_tip - 1].y > lm_list[thumb_tip - 2].y and lm_list[0].x < lm_list[3].y:
+                if lm_list[thumb_tip].y > lm_list[thumb_tip - 1].y > lm_list[thumb_tip - 2].y and lm_list[0].x < \
+                        lm_list[3].y:
                     cv2.putText(img, "DISLIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-                    print("DISLIKE")
+                    label = "DISLIKE"
+                    AI_speak(label)
                     # h, w, c = dislike_img.shape
                     # img[35:h + 35, 30:w + 30] = dislike_img
-
 
             mp_draw.draw_landmarks(img, hand_landmark,
                                    mp_hands.HAND_CONNECTIONS,
